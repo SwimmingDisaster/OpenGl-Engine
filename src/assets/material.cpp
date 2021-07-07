0bfb3ec1a3c8ba4d00d7ca6b81b1b3429c58ba13
SwimmingDisaster <mixognjen01@gmail.com>
2021-07-07 17:20:50 +0200

    Added the serialization of the new material class

--- src/assets/material.cpp
+++ src/assets/material.cpp
@@ -60,11 +60,42 @@ void Material::Serialize(YAML::Emitter& out) {
 
     out << YAML::Key << "Color" << YAML::Value << color;
 
+	for(int i = 0; i < materialProperties.size(); i++){
+		if(materialProperties[i].second.type() == typeid(float)) {
+			out << YAML::Key << ("f" + materialProperties[i].first) << YAML::Value << std::any_cast<float>(materialProperties[i].second);
+        }
+		else if(materialProperties[i].second.type() == typeid(int)) {
+			out << YAML::Key << ("i" + materialProperties[i].first) << YAML::Value << std::any_cast<int>(materialProperties[i].second);
+        }
+		else if(materialProperties[i].second.type() == typeid(glm::vec3)) {
+			out << YAML::Key << ("3" + materialProperties[i].first)<< YAML::Value << std::any_cast<glm::vec3>(materialProperties[i].second);
+        }
+		else if(materialProperties[i].second.type() == typeid(glm::vec4)) {
+			out << YAML::Key << ("4" + materialProperties[i].first) << YAML::Value << std::any_cast<glm::vec4>(materialProperties[i].second);
+        }
+	}
+
     out << YAML::EndMap;
 }
 
 void Material::Deserialize(const YAML::Node& data) {
     color = data["Color"].as<glm::vec3>();
+	for (YAML::const_iterator property=data.begin(); property != data.end();++property){
+		std::string name = property->first.as<std::string>();
+		char firstLetter = name[0];
+		if(firstLetter == 'f'){
+			materialProperties.push_back(std::make_pair(name.substr(1, name.size() - 1), property->second.as<float>()));
+		}
+		if(firstLetter == 'i'){
+			materialProperties.push_back(std::make_pair(name.substr(1, name.size() - 1), property->second.as<int>()));
+		}
+		if(firstLetter == '3'){
+			materialProperties.push_back(std::make_pair(name.substr(1, name.size() - 1), property->second.as<glm::vec3>()));
+		}
+		if(firstLetter == '4'){
+			materialProperties.push_back(std::make_pair(name.substr(1, name.size() - 1), property->second.as<glm::vec4>()));
+		}
+	}
 }
 
 

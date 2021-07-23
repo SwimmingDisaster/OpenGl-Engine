2a963e1ac7218bcbe0603f3dd14846dde2f46a38
SwimmingDisaster <mixognjen01@gmail.com>
2021-07-23 11:17:35 +0200

    Moved all 3rd party .h/.cpp files to the vendor folder

--- makeall.sh
+++ makeall.sh
@@ -3,15 +3,15 @@ mkdir -p  bin/linux/Relase
 
 mkdir -p obj
 mkdir -p obj/assets
-mkdir -p obj/GLAD
-mkdir -p obj/ImGUI
 mkdir -p obj/utils
-mkdir -p obj/imguizmo
-
-mkdir -p obj/ecs/other
-mkdir -p obj/yaml-cpp/contrib
 mkdir -p obj/core/other
 mkdir -p obj/core/ui
+mkdir -p obj/ecs/other
+
+mkdir -p obj/vendor/GLAD
+mkdir -p obj/vendor/ImGUI
+mkdir -p obj/vendor/imguizmo
+mkdir -p obj/vendor/yaml-cpp/contrib
 
 ./makepch.sh
 ./makeglad.sh
--- makeglad.sh
+++ makeglad.sh
@@ -1,2 +1,2 @@
-echo 'obj/GLAD/glad.o'
-cd obj/GLAD/ && clang -c ../../src/GLAD/glad.c -I ../../include/
+echo 'obj/vendor/GLAD/glad.o'
+cd obj/vendor/GLAD/ && clang -c ../../../src/vendor/GLAD/glad.c -I ../../../include/ -I ../../../include/vendor
